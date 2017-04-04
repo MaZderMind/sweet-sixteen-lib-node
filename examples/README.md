9e1eb6f22e63a1989dae25b70c308ecc40ef1d2f
MaZderMind <git@mazdermind.de>
2017-04-04 22:22:02 +0200

    wip: plugin architecture

---
+++ examples/README.md
@@ -0,0 +1,11 @@
+Example Scripts
+===============
+
+- `cli-control-with-websocket.js`:
+  Control sent Data-Packages (as Hex-Codes) via a Readline
+  CLI-Interface; Monitor the results on the WebUI and,
+  if available, on the Board.
+- `websocket-toggle-control.js`:
+  Demonatration of the WebUI Toggle-Plugin. Allows Toggeling
+  individual LEDs via the WebUI.
+
--- examples/websocket-toggle-control.js
+++ examples/websocket-toggle-control.js
@@ -11,21 +11,9 @@ RPiDriver.canRun()
 	.catch((err) => debug("RPi Driver can't be initialized:", err))
 
 	// always enable the WebSocketDriver
-	.then(() => api.addDriver(new WebSocketDriver()))
+	.then(() => api.addDriver(new WebSocketDriver()
+		.addPlugin('toggle')))
 
 	// setup and transmit
 	.then(() => debug('Setting up Drivers'))
-	.then(() => api.setup())
-
-	.then(() => console.log('Type 16-Bit Packet in Hex, separated by Space per Line to transmit & latch'))
-	.then(() => console.log(' ie. "FA32 FFE5 65B3 D55F"'));
-
-const rl = readline.createInterface({
-	input: process.stdin,
-	output: process.stdout
-});
-rl.on('line', (input) => {
-	let numbers = input.split(' ').map(str => parseInt(str.trim(), 16));
-	api.transmit(numbers);
-	api.latch();
-});
+	.then(() => api.setup());

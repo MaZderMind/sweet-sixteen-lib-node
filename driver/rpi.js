59b0df5bcefb13591a9fa81a962a2890a3d28ce9
MaZderMind <git@mazdermind.de>
2017-04-15 16:55:14 +0200

    rpi driver

--- driver/rpi.js
+++ driver/rpi.js
@@ -1,30 +1,64 @@
-const fs = require('fs');
 const debug = require('debug')('sweet-sixteen:RPiDriver');
 const Promise = require('bluebird');
+const Buffer = require('buffer');
+const fs = Promise.promisifyAll(require("fs"));
+const SPI = Promise.promisifyAll(require('pi-spi'));
 
 class RPiDriver {
-	static canRun() {
-		debug("Disabling RPi Driver");
-		return Promise.reject("not on an RPi")
+	constructor() {
+		this.GPIO_LATCH = 22;
 	}
+
+	static canRun() {
+		return fs.readFileAsync('/proc/cpuinfo', {encoding: 'ascii'})
+			.then((content) => {
+				const isBCM2708 = content.indexOf('BCM2708') !== -1;
+				const isBCM2709 = content.indexOf('BCM2709') !== -1;
+				const isPi = isBCM2708 || isBCM2709;
+
+				return isPi ? Promise.resolve() : Promise.reject('not on an RPi');
+			});
+	}
+
 	setApi(api) {
 		this.api = api;
 		return this;
 	}
 
 	setup() {
-		debug('RPi Setup');
-		return Promise.reject("RPi Setup failed")
+		return Promise.all([
+			this.initializeSPI(),
+			this.openLatchGpio(),
+			this.openPWMGpio(),
+		]);
 	}
 
 	transmit(data) {
-		debug('RPi Transmit');
-		return Promise.reject("RPi Transmit failed")
+		const buf = new Buffer.alloc(this.api.shiftRegisterCount * 2);
+		data.forEach(register => buf.readUInt16BE(register));
+		return SPI.transmitAsync(buf, buf.length);
 	}
 
 	latch() {
-		debug('RPi Latch');
-		return Promise.reject("RPi Latch failed")
+		return fs.writeFileAsync('/sys/class/gpio/gpio'+this.GPIO_LATCH+'/value', '1')
+			.then(() => fs.writeFileAsync('/sys/class/gpio/gpio'+this.GPIO_LATCH+'/value', '0'));
+	}
+
+	initializeSPI() {
+		debug('SPI: initializing');
+		SPI.initialize("/dev/spidev0.0");
+		debug('SPI: done');
+		return Promise.resolve();
+	}
+
+	openLatchGpio() {
+		return fs.writeFileAsync('/sys/class/gpio/export', this.GPIO_LATCH)
+			.then(() => fs.writeFileAsync('/sys/class/gpio/gpio'+this.GPIO_LATCH+'/direction', 'out'))
+			.then(() => fs.writeFileAsync('/sys/class/gpio/gpio'+this.GPIO_LATCH+'/value', '0'));
+	}
+
+	openPWMGpio() {
+		return Promise.resolve();
 	}
 }
 
